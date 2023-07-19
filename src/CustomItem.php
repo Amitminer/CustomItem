b7bef6b6a3796227f971c9413a632f5d14e5ba94
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-07-19 21:51:49 +0530

    Add files via upload

---
+++ src/CustomItem.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AmitxD\CustomItem;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use AmitxD\CustomItem\Manager\FormManager;
+
+class CustomItem extends PluginBase {
+
+    public function onLoad(): void {
+        $this->getLogger()->info("§aEnabled CustomItem!");
+    }
+
+    public function onEnable(): void {
+       // $this->saveDefaultConfig();
+        $this->runEvents();
+    }
+
+    /**
+     * Executes a command on the command sender's behalf.
+     * @param CommandSender $sender The sender of the command.
+     * @param Command $command The command that was executed.
+     * @param string $label The alias or label used to execute the command.
+     * @param array $args An array of arguments passed to the command.
+     * @return bool
+     */
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        switch ($command->getName()) {
+            case "customitems":
+                FormManager::displayCustomItemsForm($sender);
+                break;
+        }
+        return true;
+    }
+    
+    private function runEvents(): void {
+        $this->callEvent("FrezzingSword");
+        $this->callEvent("TeleportationSword");
+        $this->callEvent("TimeController");
+    }
