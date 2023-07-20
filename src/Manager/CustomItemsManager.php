6177782050a090f75764a18511ed9e7711f39472
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-07-20 21:57:02 +0530

    fixed typo._

--- src/Manager/CustomItemsManager.php
+++ src/Manager/CustomItemsManager.php
@@ -17,7 +17,7 @@ class CustomItemsManager {
      * Get the Frezzing Sword custom item and give it to the player.
      * @param mixed $player The player to whom the item will be given.
      */
-    public static function giveFrezzingSword($player): void {
+    public static function getFrezzing($player): void {
         $item = ItemManager::getCustomItem("diamond_sword");
         $item->setName("§r§b§lFREZZING SWORD");
         $item->setLore(["§aFrezz Players for 5 Seconds!", "\n§cCooldown: 15 Seconds\n§r§l§9RARE"]);
@@ -30,7 +30,7 @@ class CustomItemsManager {
      * Get the Teleportation Sword custom item and give it to the player.
      * @param mixed $player The player to whom the item will be given.
      */
-    public static function giveTeleportationSword($player): void {
+    public static function getTeleportationSword($player): void {
         $item = ItemManager::getCustomItem("netherite_sword");
         $item->setName("§r§eTeleportation-Sword");
         $item->setLore(["§r§bRight Click To Teleport.\n\n§r§9RARE"]);
@@ -43,7 +43,7 @@ class CustomItemsManager {
      * Get the Time Controller custom item and give it to the player.
      * @param mixed $player The player to whom the item will be given.
      */
-    public static function giveTimeController($player): void {
+    public static function getTimeController($player): void {
         $item = ItemManager::getCustomItem("compass");
         $item->setName("§r§gTime Controller");
         $item->setLore(["§r§bRight Click To Change Time of Your World!.\n\n§r§9RARE"]);
