7462273cad9b5d892d3048d7dc9633729dd4961f
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-08-11 12:18:00 +0530

    added custom enchant..

---
+++ src/Utils/EnchantmentIds.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AmitxD\CustomItem\Utils;
+
+class EnchantmentIds{
+    
+    public const FREEZING = 40;
+    
+    public const TELEPORTATION = 41;
+    
+    public const TIMESTOPPER = 42;
+    
+    public const LIGHTNING = 43;
+}
--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -6,17 +6,23 @@ namespace AmitxD\CustomItem\Utils;
 
 use pocketmine\item\Item;
 use pocketmine\item\enchantment\StringToEnchantmentParser;
+use pocketmine\data\bedrock\EnchantmentIdMap;
+use pocketmine\item\enchantment\ItemFlags;
+use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\LegacyStringToItemParser;
 use pocketmine\item\LegacyStringToItemParserException;
 use pocketmine\item\StringToItemParser;
+use AmitxD\CustomItem\Utils\EnchantmentIds;
 
 class Utils {
 
+    public const FAKE_ENCH_ID = -1;
+
     /**
-     * Converts a string representation of an item to an Item instance.
-     * @param string $input The string representation of the item.
-     * @return Item|null The Item instance if the conversion was successful, or null if failed.
-     */
+    * Converts a string representation of an item to an Item instance.
+    * @param string $input The string representation of the item.
+    * @return Item|null The Item instance if the conversion was successful, or null if failed.
+    */
     public static function stringToItem(string $input): ?Item {
         $string = strtolower(str_replace([' ', 'minecraft:'], ['_', ''], trim($input)));
         try {
@@ -28,12 +34,19 @@ class Utils {
     }
 
     /**
-     * Converts a string representation of an enchantment to an Enchantment instance.
-     * @param string $name The name of the enchantment.
-     * @return Enchantment|null The Enchantment instance if the conversion was successful, or null if failed.
-     */
-    public static function stringToEnchantment(string $name){
+    * Converts a string representation of an enchantment to an Enchantment instance.
+    * @param string $name The name of the enchantment.
+    * @return Enchantment|null The Enchantment instance if the conversion was successful, or null if failed.
+    */
+    public static function stringToEnchantment(string $name) {
         $enchantment = StringToEnchantmentParser::getInstance()->parse($name);
         return $enchantment ?? null;
+    }
+
+    public static function createEnchantment(string $name, int $id): void {
+        // i want to pass 
+        if (!empty($name)) {
+            EnchantmentIdMap::getInstance()->register($id, new Enchantment(strtolower($name), 1, ItemFlags::ALL, ItemFlags::NONE, 1));
+        }
     }
 }
