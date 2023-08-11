c9452ae525f215fd8d73ec0ddfbf5d0a406388da
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-08-11 12:18:42 +0530

    updating item manager..added lighting sword

--- src/Manager/CustomItemsManager.php
+++ src/Manager/CustomItemsManager.php
@@ -6,49 +6,67 @@ namespace AmitxD\CustomItem\Manager;
 
 use AmitxD\CustomItem\Utils\Utils;
 use AmitxD\CustomItem\Manager\ItemManager;
+use AmitxD\CustomItem\Utils\EnchantmentIds;
+use AmitxD\CustomItem\CustomItem;
 
 class CustomItemsManager {
-    
+
     public function __construct() {
         // NOOP (No operation)
     }
-    
+
     /**
-     * Get the Frezzing Sword custom item and give it to the player.
-     * @param mixed $player The player to whom the item will be given.
-     */
+    * Get the Frezzing Sword custom item and give it to the player.
+    * @param mixed $player The player to whom the item will be given.
+    */
     public static function getFrezzing($player): void {
         $item = ItemManager::getCustomItem("diamond_sword");
-        $item->setName("§r§b§lFREZZING SWORD");
-        $item->setLore(["§aFrezz Players for 5 Seconds!", "\n§cCooldown: 15 Seconds\n§r§l§9RARE"]);
-        $item->setEnchantment("unbreaking", 3);
-        $item->setTag("FrezzingSword");
+        $item->setName("§r§b§lFREEZING SWORD");
+        $item->setLore(["§aFreezz Players for 5 Seconds!", "\n§cCooldown: 15 Seconds\n§r§l§9RARE"]);;
+        $item->setEnchantment(EnchantmentIds::FREEZING, 1);
+        //var_dump($ec);
+        $item->setTag("FreezingSword");
         $player->getInventory()->addItem($item->getItem());
     }
 
     /**
-     * Get the Teleportation Sword custom item and give it to the player.
-     * @param mixed $player The player to whom the item will be given.
-     */
+    * Get the Teleportation Sword custom item and give it to the player.
+    * @param mixed $player The player to whom the item will be given.
+    */
     public static function getTeleportationSword($player): void {
         $item = ItemManager::getCustomItem("netherite_sword");
         $item->setName("§r§eTeleportation-Sword");
         $item->setLore(["§r§bRight Click To Teleport.\n\n§r§9RARE"]);
-        $item->setEnchantment("unbreaking", 3);
+        $item->setEnchantment(EnchantmentIds::TELEPORTATION, 1);
         $item->setTag("TP-Sword");
         $player->getInventory()->addItem($item->getItem());
     }
 
     /**
-     * Get the Time Controller custom item and give it to the player.
-     * @param mixed $player The player to whom the item will be given.
-     */
+    * Get the Time Controller custom item and give it to the player.
+    * @param mixed $player The player to whom the item will be given.
+    */
+
     public static function getTimeController($player): void {
         $item = ItemManager::getCustomItem("compass");
         $item->setName("§r§gTime Controller");
         $item->setLore(["§r§bRight Click To Change Time of Your World!.\n\n§r§9RARE"]);
-        $item->setEnchantment("unbreaking", 3);
+        $item->setEnchantment(EnchantmentIds::TIMESTOPPER, 1);
         $item->setTag("TimeController");
+        $player->getInventory()->addItem($item->getItem());
+    }
+
+    public static function getLightning($player): void {
+        $item = ItemManager::getCustomItem("gold_sword");
+        $item->setName("§r§b§lLIGHTNING-SWORD");
+        $item->setLore([
+            "§eUnleash lightning on foes!",
+            "§7Damage over time on hit.",
+            "\n§cCooldown: 15s\n\n§r§9RARE"
+        ]);
+        $item->setEnchantment(EnchantmentIds::LIGHTNING, 1);
+        //var_dump($ec);
+        $item->setTag("LightningSword");
         $player->getInventory()->addItem($item->getItem());
     }
 }
--- src/Manager/ItemManager.php
+++ src/Manager/ItemManager.php
@@ -6,78 +6,89 @@ namespace AmitxD\CustomItem\Manager;
 
 use AmitxD\CustomItem\Utils\Utils;
 use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\data\bedrock\EnchantmentIdMap;
+use AmitxD\CustomItem\Utils\EnchantmentsIds;
 use pocketmine\item\Item;
 
 class ItemManager {
-    
+
     private $item;
-    
+
     /**
-     * ItemManager constructor.
-     * @param Item $item The base item instance to manage.
-     */
+    * ItemManager constructor.
+    * @param Item $item The base item instance to manage.
+    */
     private function __construct(Item $item) {
         $this->item = $item;
     }
-    
+
     /**
-     * Get a custom item manager instance for the specified item name.
-     * @param string $item The name of the custom item.
-     * @return ItemManager
-     */
+    * Get a custom item manager instance for the specified item name.
+    * @param string $item The name of the custom item.
+    * @return ItemManager
+    */
     public static function getCustomItem(string $item): self {
         $itemInstance = Utils::stringToItem($item);
         return new self($itemInstance);
     }
 
     /**
-     * Set the custom name for the item.
-     * @param string $name The custom name to set.
-     * @return ItemManager
-     */
+    * Set the custom name for the item.
+    * @param string $name The custom name to set.
+    * @return ItemManager
+    */
     public function setName(string $name): self {
         $this->item->setCustomName($name);
         return $this;
     }
 
     /**
-     * Set the lore for the item.
-     * @param array $lore An array of strings representing the item's lore.
-     * @return ItemManager
-     */
+    * Set the lore for the item.
+    * @param array $lore An array of strings representing the item's lore.
+    * @return ItemManager
+    */
     public function setLore(array $lore): self {
         $this->item->setLore($lore);
         return $this;
     }
 
     /**
-     * Add an enchantment to the item.
-     * @param string $enchantment The name of the enchantment to add.
-     * @param int $level The level of the enchantment (default: 1).
-     * @return ItemManager
-     */
-    public function setEnchantment(string $enchantment, int $level = 1): self {
-        $enchant = Utils::stringToEnchantment($enchantment);
-        if ($enchant !== null) {
-            $this->item->addEnchantment(new EnchantmentInstance($enchant, $level));
+    * Add an enchantment to the item.
+    * @param string $enchantment The name of the enchantment to add.
+    * @param int $level The level of the enchantment (default: 1).
+    * @return ItemManager
+    */
+    public function setEnchantment(mixed $enchantment, int $level = 1): self {
+        if (is_string($enchantment)) {
+            $enchant = Utils::stringToEnchantment($enchantment);
+        } elseif (is_int($enchantment)) {
+            $enchant = EnchantmentIdMap::getInstance()->fromId($enchantment);
+        } else {
+            throw new \InvalidArgumentException("An error occurred while adding enchantment on item.");
         }
+
+        if ($enchant !== null) {
+            $enchantmentInstance = new EnchantmentInstance($enchant, $level);
+            $this->item->addEnchantment($enchantmentInstance);
+        }
+
         return $this;
     }
 
     /**
-     * Set a custom tag for the item.
-     * @param string $nameTag The name of the custom tag.
-     * @return ItemManager
-     */
+    * Set a custom tag for the item.
+    * @param string $nameTag The name of the custom tag.
+    * @return ItemManager
+    */
     public function setTag(string $nameTag): self {
         $this->item->getNamedTag()->setString($nameTag, $nameTag);
         return $this;
     }
 
     /**
-     * Get the final customized item after all modifications.
-     * @return Item The customized item instance.
-     */
+    * Get the final customized item after all modifications.
+    * @return Item The customized item instance.
+    */
     public function getItem(): Item {
         return $this->item;
     }
